5540d04f50338f950a00ffbccb2adc77bdeaed86
snefer1 <94827256+snefer1@users.noreply.github.com>
2021-11-22 09:20:52 +0100

    Add files via upload
    pierwszy plik

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace space_invaders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
